0043e1138418c4ebeb10d7707e8082db8615c736
martin.mueller <m.mueller@unimed.de>
2020-09-24 07:20:52 +0200

    translate Kontakt

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -3,7 +3,7 @@ import { MapPin, Smartphone, Mail } from 'react-feather'
 import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
-import FormSimpleAjax from '../components/FormSimpleAjax'
+// import FormSimpleAjax from '../components/FormSimpleAjax'
 import Content from '../components/Content'
 import GoogleMap from '../components/GoogleMap'
 import Layout from '../components/Layout'
@@ -34,7 +34,7 @@ export const ContactPageTemplate = ({
             {address && (
               <a
                 className="Contact--Details--Item"
-                href={`https://www.google.com.au/maps/search/${encodeURI(
+                href={`https://www.google.de/maps/search/${encodeURI(
                   address
                 )}`}
                 target="_blank"
@@ -56,9 +56,9 @@ export const ContactPageTemplate = ({
           </div>
         </div>
 
-        <div>
+        {/* <div>
           <FormSimpleAjax name="Simple Form Ajax" />
-        </div>
+        </div> */}
       </div>
     </section>
 
