054fa6fe08eca2da747925ccc60d869274dc809d
martin.mueller <m.mueller@unimed.de>
2020-09-24 06:31:59 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,15 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
+    {/* <h2 className="taCenter">
       Follow us{' '}
       <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
+    </h2> */}
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          © Copyright {new Date().getFullYear()} All rights reserved.
         </span>
       </div>
     </footer>
