b505810aa134c942c8ebb224a2f3660206c6d18d
martin.mueller <m.mueller@unimed.de>
2020-09-25 22:50:19 +0200

    Links für Events

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,15 +44,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'lkgludwigslust',
+        short_name: 'lkgludwigslust',
         start_url: '/',
         background_color: '#00C2BD',
         theme_color: '#00C2BD',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
-        icon: `${__dirname}/favicon.ico` // This path is relative to the root of the site.
+        icon: `${__dirname}/static/favicon.png` // This path is relative to the root of the site.
         // icon: `${__dirname}/static/images/logo.png` // This path is relative to the root of the site.
       }
     },
